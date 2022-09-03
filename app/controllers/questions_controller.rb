be9c344d026b273bdf7a20a08a952bafe4200d4d
Пастухов Александр Викторович <alex@MacBook-Pro-Pastuhov.local>
2022-09-03 18:05:40 +0300

    add less3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,48 @@
 class QuestionsController < ApplicationController
+
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new questions_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @question = Question.find_by id: params[:id]
+  end
+
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update questions_params
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question = Question.find_by id: params[:id]
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def show
+    @question = Question.find_by id: params[:id]
+  end
+
+  private
+  def questions_params
+    params.require(:question).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :questions
+  # resources :questions, only: %i[index new edit create update destroy show]
   root 'pages#index'
-  get '/questions', to: 'questions#index'
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # post '/questions', to: 'questions#create'
+  # get '/questions/:id/edit', to: 'questions#edit'
 end
