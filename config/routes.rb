7d6d278cceaa4534255b6d810f8f9a1df6b92834
Пастухов Александр Викторович <alex@MacBook-Pro-Pastuhov.local>
2022-09-13 15:35:54 +0300

    add less6

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   resources :questions do
-    resources :answers, only: %i[create destroy]
+    resources :answers, except: %i[new show]
   end
-  # resources :questions, only: %i[index new edit create update destroy show]
   root 'pages#index'
-  # get '/questions', to: 'questions#index'
-  # get '/questions/new', to: 'questions#new'
-  # post '/questions', to: 'questions#create'
-  # get '/questions/:id/edit', to: 'questions#edit'
 end
