848b9ce3133f80ac08ef543ac78d7a84d7a2236f
Пастухов Александр Викторович <alex@MacBook-Pro-Pastuhov.local>
2022-09-20 19:27:24 +0300

    add less9

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -27,7 +27,7 @@ module Authentication
     end
 
     def require_no_authentication
-      return if !user_signed_in?
+      return if user_signed_in?
       flash[:warning] = "You are already signed in!"
       redirect_to root_path
     end
